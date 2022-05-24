d34e8641c9cc34833f764e755bc8de2f597eef76
mixed_nuts_nuts <french0719@gmail.com>
2022-05-24 23:17:25 +0900

    second

--- chapter1.py
+++ chapter1.py
@@ -1,8 +1,14 @@
 """関数設計"""
 # 1 関数名だけで処理を想像可能にする!
+from dataclasses import dataclass
+from distutils.command.build_scripts import first_line_re
+from email.policy import default
+
+
 def write_item_csv(item):
     with open("item.csv", mode="w") as f:
         f.write(item.name)
+
 
 # 2 関数名は具体的な意味の英単語！　→ より狭い意味！
 # get よりは fetch, calc, aggregate(複数の情報から集計) etc..
@@ -10,12 +16,14 @@ def write_item_csv(item):
 # load, fetch, retrieve, search, calc, increase, decrease, merge, render, filter, aggregate, build, escape
 # dump, create, update, patch, remove, sync, memoize, publish, notify, flatten, minimize, validate
 
+
 # 3 関数名から想像できる型の戻り値
 # is_, has_, enabled_, _activated, _confirmed などはboolを返すと推測できるから、使う時注意！
 # bool型の関数は副作用が無いようにも注意！(外部アクセス、データ保存・読み込み、値の変換 etc.. )
 # → if is_valid のように使える！！
 def is_valid(name):
     return not name.endswith(".txt")
+
 
 # 4 副作用のない関数にまとめる！
 # →同じ入力を与えると常に同じ結果が出力するように！　→　テストしやすい！
@@ -34,6 +42,99 @@ def update_article(article, title, body):
     article.save()
     return article
 
+
 # 5 関数を意味づけできるように関数化！　むやみに処理のまとまりだけで分けたりしない。。
 # プログラムの意図が分かりずらい。　関数の再利用が難しい。　単体テストしずらい。
 # → 再利用性、処理の意味で分けるように！
+
+
+# 6　デフォルト引数にリスト・辞書は設定しない
+# デフォルト引数の値は関数呼び出しのたびに初期化されない！
+# ミュータブル(更新可能)な値はデフォルト引数にしてはいけない→ 該当：リスト、辞書、集合
+
+# ↓　これだと何回fooを呼び出しても"Hi"がreturnされる！
+def foo(values = None):
+    values = values or []
+    values.append("Hi")
+    return values
+
+
+# 7 コレクションを引数にせず、str or int で受け取る！
+# Ex. 失敗例(消費税を計算したい場合でも、毎度["price"]をキーに持つ辞書を用意しないといけない。。)
+# → 関数の再利用性が低くなる！
+def calc_tax_included(item, tax_rate = 0.1):
+    return item["price"] * (1 + tax_rate)
+# ↓ みたいにコレクションでない値で受け取る！
+def calc_tax_included(price, tax_rate = 0.1):
+    return price * (1 + tax_rate)
+ 
+# 上だとテストが楽！（１００を渡すだけで良い！）
+def test_calc_tax_included():
+    assert calc_tax_included(100) == 110
+
+# もし「商品」値を扱うときに、税込計算を頻繁にするならclass定義し、propertyから税込価格をreturnする！
+class Item:
+    # ...
+    @property
+    def price_including_tax(self):
+        return calc_tax_included(self.price)
+
+
+# 8 indexに意味を持たせない！
+# 間に値が入ると意味が壊れる。index番号を覚えていなければいけない。
+# タプルで管理せず、辞書やクラスにする！！
+@dataclass
+class Sale:
+    sale_id : int
+    item_id : int
+    user_id : int
+    amount : int
+    sold_at : datetime
+
+def validate_sales(sale):
+    if not item_exists(sale.item_id):
+        raise # ...
+    if sale.amount < 1:
+        raise # ..
+
+# ループの時も気をつける　→ イテレータを使う！
+# Ex .(bad)
+for idx in range(len(items)):
+    items[idx]
+# Ex .(good)
+for item in items:
+    item
+# indexが明示的に必要な場合　→ enumerate
+for n, item in enumerate(items, start=1):
+    print(n, "個目を処理中...")
+
+
+# 9 引数に可変長引数 (*args, *kwargs) を使用しない！
+# → 引数に間違えて値を与えてもエラーを吐かないから！
+# → 個別の引数で指定！
+class User:
+    def __init__(self, name, mail=None):
+        self.name = name
+        self.mail = mail
+
+# 可変長引数を使うのは、どんな値が来ても良い関数のみ！！
+def as_json(obj, **fields):
+    data = {getattr(obj, key, default) for key, default in fields.items()}
+    return json.dumps(data)
+
+some_obj = get_some_obj()
+json_ste = as_json(some_obj, first_name = "John", last_name = "Doe")
+
+
+# 10 コメントにはwhyを書く
+# 関数の仕様を書く場合はコメントではなくdocstringに書く！
+def do_something(users):
+    """~~する処理
+
+    複数のユーザに対し<do_something>を行う。
+    ~~の場合に〜〜だから、ユーザのデータを更新する必要がある！
+    """
+    # SQLの実行回数を減らすため、以下のループは別関数に分離せず処理する。
+
+# 処理の意味と、なぜそう書くのか？？を書く。　
+# なぜこう処理しないのか？？の説明！！
